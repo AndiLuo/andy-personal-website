87f9e81e5e32ef0ce2b7016e5457cb26ca946058
unknown <aluo5731@gmail.com>
2021-03-15 14:35:35 -0700

    Updates

---
+++ precache-manifest.5d55aab4585de8cb697fac6c47d12940.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49cfe52a8fa64c69e82b50530a59341",
+    "url": "/andy-personal-website/index.html"
+  },
+  {
+    "revision": "328ff5e2172d63145b0a",
+    "url": "/andy-personal-website/static/css/main.2ce6b4bb.chunk.css"
+  },
+  {
+    "revision": "d879efcba2ed5f86582b",
+    "url": "/andy-personal-website/static/js/2.c62c6f7d.chunk.js"
+  },
+  {
+    "revision": "857d129e4a5f4f33c7a4a143bf79109c",
+    "url": "/andy-personal-website/static/js/2.c62c6f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328ff5e2172d63145b0a",
+    "url": "/andy-personal-website/static/js/main.9ea4ceb0.chunk.js"
+  },
+  {
+    "revision": "d386615de4185b930ba9",
+    "url": "/andy-personal-website/static/js/runtime-main.7d7fd9ab.js"
+  },
+  {
+    "revision": "70e1c4a9d0ddeb3c68c3406d3c38aa9c",
+    "url": "/andy-personal-website/static/media/agile.70e1c4a9.png"
+  },
+  {
+    "revision": "62e740a687b297fad1a3eb7f3a396f29",
+    "url": "/andy-personal-website/static/media/amazon.62e740a6.png"
+  },
+  {
+    "revision": "4cbc497bbc1a0896e523ce71a8dc9d32",
+    "url": "/andy-personal-website/static/media/andy.4cbc497b.jpg"
+  },
+  {
+    "revision": "29c8c83b835575979dc96c5a34cebb71",
+    "url": "/andy-personal-website/static/media/mern.29c8c83b.gif"
+  },
+  {
+    "revision": "b6eba8a871ac74329c23476c34956333",
+    "url": "/andy-personal-website/static/media/mysql.b6eba8a8.png"
+  },
+  {
+    "revision": "48e3cf2c41ea30829054779740db330d",
+    "url": "/andy-personal-website/static/media/node.48e3cf2c.png"
+  },
+  {
+    "revision": "46ed9f656f2e2852e452466363acc68b",
+    "url": "/andy-personal-website/static/media/patrick-fore.46ed9f65.jpg"
+  },
+  {
+    "revision": "22284ad7b7210df2e08d708758b6c4e8",
+    "url": "/andy-personal-website/static/media/python.22284ad7.png"
+  },
+  {
+    "revision": "0ef5389396a1d58c77ebd681ddf52b13",
+    "url": "/andy-personal-website/static/media/react.0ef53893.png"
+  },
+  {
+    "revision": "c0b3c4abc220a6a4b1219355588c560e",
+    "url": "/andy-personal-website/static/media/shoppingCart.c0b3c4ab.gif"
+  }
+]);
