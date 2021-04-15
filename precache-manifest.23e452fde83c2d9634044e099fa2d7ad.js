1afc35ea00d51c9e8325a90db4cd7a6b79f4e85e
unknown <aluo5731@gmail.com>
2021-04-15 16:25:55 -0700

    Updates

---
+++ precache-manifest.23e452fde83c2d9634044e099fa2d7ad.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a8f950c8a636be5c0fd0b97a2f0eac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1a21710e3e5e3aca98b",
+    "url": "/static/css/main.2ce6b4bb.chunk.css"
+  },
+  {
+    "revision": "d20d339ca149c6f3b148",
+    "url": "/static/js/2.dbc6e4e1.chunk.js"
+  },
+  {
+    "revision": "ca2abe649ff858defa21eaf931086938",
+    "url": "/static/js/2.dbc6e4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a21710e3e5e3aca98b",
+    "url": "/static/js/main.22b00b3e.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "70e1c4a9d0ddeb3c68c3406d3c38aa9c",
+    "url": "/static/media/agile.70e1c4a9.png"
+  },
+  {
+    "revision": "62e740a687b297fad1a3eb7f3a396f29",
+    "url": "/static/media/amazon.62e740a6.png"
+  },
+  {
+    "revision": "16a7c3c7d3b7f7bc21b0ff4fe8c9cf3c",
+    "url": "/static/media/andy_2.16a7c3c7.jpg"
+  },
+  {
+    "revision": "b38578854caa86d9af41a092e13a93c1",
+    "url": "/static/media/graybg.b3857885.png"
+  },
+  {
+    "revision": "29c8c83b835575979dc96c5a34cebb71",
+    "url": "/static/media/mern.29c8c83b.gif"
+  },
+  {
+    "revision": "b6eba8a871ac74329c23476c34956333",
+    "url": "/static/media/mysql.b6eba8a8.png"
+  },
+  {
+    "revision": "48e3cf2c41ea30829054779740db330d",
+    "url": "/static/media/node.48e3cf2c.png"
+  },
+  {
+    "revision": "22284ad7b7210df2e08d708758b6c4e8",
+    "url": "/static/media/python.22284ad7.png"
+  },
+  {
+    "revision": "0ef5389396a1d58c77ebd681ddf52b13",
+    "url": "/static/media/react.0ef53893.png"
+  },
+  {
+    "revision": "c0b3c4abc220a6a4b1219355588c560e",
+    "url": "/static/media/shoppingCart.c0b3c4ab.gif"
+  },
+  {
+    "revision": "f10f4eecbafe16d7a2b50a8e06d121f8",
+    "url": "/static/media/thomas.f10f4eec.gif"
+  }
+]);
